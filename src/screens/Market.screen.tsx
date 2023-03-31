8dcb3cf9463cd10dca3e0b49f2d40a53fe8aa097
Thorben <38816229+tklein1801@users.noreply.github.com>
2023-03-31 18:55:43 +0200

    feat: Update `Market.screen`

--- src/screens/Market.screen.tsx
+++ src/screens/Market.screen.tsx
@@ -1,18 +1,73 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import { ActivityIndicator, Avatar, Card, Divider, List, Text } from 'react-native-paper';
+import { Layout } from '../components/Layout/Layout.component';
+import { PanthorService } from '../services';
+import { formatter } from '../services/CurrencyFormat.service';
+import { MarketItem } from '../types';
 
 export const MarketScreen = () => {
+  const id = React.useId();
+  const [loading, setLoading] = React.useState(true);
+  const [refreshing, setRefreshing] = React.useState(false);
+  const [items, setItems] = React.useState<MarketItem[]>([]);
+
+  const handler = {
+    fetchData: async () => {
+      try {
+        const result = await PanthorService.getMarket(1);
+        setItems(result);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onRefresh: () => {
+      setRefreshing(true);
+      handler.fetchData().finally(() => setRefreshing(false));
+    },
+  };
+
+  React.useEffect(() => {
+    handler.fetchData().finally(() => setLoading(false));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Markt</Text>
-    </View>
+    <Layout
+      refreshControl={{
+        refreshing: refreshing,
+        onRefresh: handler.onRefresh,
+      }}
+    >
+      {loading ? (
+        <Card elevation={1} style={{ padding: 16 }}>
+          <ActivityIndicator animating={true} />
+        </Card>
+      ) : (
+        <Card>
+          <List.Section>
+            {items.map((item, index) => (
+              <React.Fragment key={id + '-market-' + item.item}>
+                {index !== 0 && <Divider />}
+                <List.Item
+                  title={item.localized}
+                  left={() => (
+                    <Avatar.Image
+                      size={40}
+                      source={{ uri: item.getImageUrl() }}
+                      style={{ marginLeft: 16, backgroundColor: 'transparent' }}
+                    />
+                  )}
+                  right={() => (
+                    <View style={{ display: 'flex', justifyContent: 'center' }}>
+                      <Text>{formatter.format(item.price)}</Text>
+                    </View>
+                  )}
+                />
+              </React.Fragment>
+            ))}
+          </List.Section>
+        </Card>
+      )}
+    </Layout>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
