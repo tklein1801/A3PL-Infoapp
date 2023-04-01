e3235b9e69f4747f2cff9795735958afcc7d61b3
Thorben <38816229+tklein1801@users.noreply.github.com>
2023-04-01 22:10:41 +0200

    build: Resolve some issues during building

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { Provider as PaperProvider } from 'react-native-paper';
 import Main from './src/Main';
 import { Theme } from './src/theme/theme';
 import { registerRootComponent } from 'expo';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   React.useEffect(() => {
@@ -12,6 +13,8 @@ export default function App() {
 
   return (
     <PaperProvider theme={Theme}>
+      {/* Sets the color of the status bar text. Default value is "auto" which picks the appropriate value according to the active color scheme, eg: if your app is dark mode, the style will be "light". */}
+      <StatusBar style={Theme.dark ? "light" : "dark"} />
       <Main />
     </PaperProvider>
   );
